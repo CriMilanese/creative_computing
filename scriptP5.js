2da9bec4adeac1189ed7c5cb212ce2add2a0da9f
CriMilanese <cmilanese@pop-os.localdomain>
2019-09-19 15:42:45 +0900

    first simple commit

---
+++ scriptP5.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(windowWidth, windowWidth);
+  background(0, 0, 0);
+  var r, g, b;
+}
+
+function draw() {
+  var size = random(5, 50);
+  r = random(1, 255);
+  g = random(1, 255);
+  b = random(1, 255);
+  
+  if(mouseIsPressed){
+    
+    fill(r, g, b);
+    ellipse(pmouseX, pmouseY, size, size);
+  } else if(keyIsPressed){
+    background(0,0,0);
+  }
+}
