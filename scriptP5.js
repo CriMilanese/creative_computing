8c6d22dc9ddabfd124a461f076f0a328e15d493c
CriMilanese <cmilanese@pop-os.localdomain>
2019-09-25 19:49:51 +0900

    local img file problem

--- scriptP5.js
+++ scriptP5.js
@@ -1,7 +1,22 @@
+var r, g, b;
+var menu_open = false;
+var button, imgTools;
+
+function preload(){
+  //this file is used to load images and sound and other thinks
+  // and it happens to eb run even before setup
+  imgTools = loadImage('images/burger_button.jpg');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowWidth);
+  createCanvas(windowWidth, windowHeight);
   background(0, 0, 0);
-  var r, g, b;
+  menu_open = false;
+  button = createButton('Tools');
+  // imgTools.style();
+  button.position(10,10);
+  button.mousePressed(openNav);
+  // imgTools.parent(button);
 }
 
 function draw() {
@@ -10,11 +25,22 @@ function draw() {
   g = random(1, 255);
   b = random(1, 255);
 
-  if(mouseIsPressed){
-
+  if(mouseIsPressed && menu_open == false){
     fill(r, g, b);
-    ellipse(pmouseX, pmouseY, size, size);
+    // ellipse(pmouseX, pmouseY, size, size);
+    imageMode(CENTER);
+    image(imgTools, pmouseX, pmouseY);
   } else if(keyIsPressed){
     background(0,0,0);
   }
+}
+
+function openNav() {
+  menu_open = true;
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  menu_open = false;
 }
