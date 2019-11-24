55098dddc5c1981ec692bb44ee3919961238c929
CriMilanese <cmilanese@pop-os.localdomain>
2019-11-24 17:32:28 +0900

    turning triangles and dropping bombs

--- typo_graph/agent.js
+++ typo_graph/agent.js
@@ -3,26 +3,57 @@ function Agent(x, y){
   this.target = createVector(x, y);
   this.vel = p5.Vector.random2D();
   this.acc = createVector();
-  this.r = 8;
+  this.angle = 8;
+  this.radius = 10;
   this.maxSpeed = 5;
+  this.maxForce = 1;
+  this.c = color(random(255), random(255), random(255));
 }
 
+// change its acceleration (and therefore speed) based on distance from target
+// increment angle of rotation
 Agent.prototype.update = function() {
-  this.pos.add(this.vel);
   this.vel.add(this.acc);
+  this.pos.add(this.vel);
   this.acc.mult(0);
+  this.angle  += PI/60;
+  if(this.angle > 2*PI){
+    angle=0;
+  }
 }
 
+// draw a triangle and its shadow
 Agent.prototype.show = function(){
-  stroke(255);
-  strokeWeight(3);
-  ellipse(this.pos.x, this.pos.y, 3, 3);
+  let b = createVector(this.radius*cos(PI/6), this.radius*sin(PI/6));
+  let a = createVector(this.radius*cos(PI*5/6), this.radius*sin(PI*5/6));
+  let c = createVector(this.radius*cos(PI*3/2), this.radius*sin(PI*3/2));
+  noStroke();
+  push();
+  translate(this.pos.x+2, this.pos.y+2);
+  rotate(this.angle);
+  fill(0, 0, 0, 50);
+  triangle(a.x, a.y, b.x, b.y, c.x, c.y);
+  pop();
+  push();
+  translate(this.pos.x, this.pos.y);
+  rotate(this.angle);
+  fill(this.c);
+  triangle(a.x, a.y, b.x, b.y, c.x, c.y);
+  pop();
 }
 
+// calculate vector towards target
+// limit the speed based on distance from target
 Agent.prototype.seek = function(t){
   var desired = p5.Vector.sub(t, this.pos);
-  desired.limit(this.maxSpeed);
+  var d = desired.mag();
+  var speed = this.maxSpeed;
+  if(d < 400){
+    speed = map(d, 0, 400, 0, this.maxSpeed);
+  }
+  desired.setMag(speed);
   var steer = p5.Vector.sub(desired, this.vel);
+  steer.limit(this.maxForce)
   return steer;
 }
 
--- typo_graph/script.js
+++ typo_graph/script.js
@@ -1,26 +1,25 @@
 var font;
-var txt = "magnameo";
+var txt = "Cris";
 let agents = [];
+let textInput;
+let textLoc;
+let points;
 
 function preload(){
   font = loadFont('Arvo-Bold.ttf');
 }
+
 function setup() {
   // use full screen size
-  cnv = createCanvas(displayWidth, displayHeight);
+  cnv = createCanvas(windowWidth, windowHeight);
+  textInput = createInput('insert here..');
+  textInput.position(50, 50);
   textFont(font);
-  textAlign(CENTER);
-  textSize(256);
-  var points = font.textToPoints(txt, 150, height/2, 256);
-  for (var i = 0; i < points.length; i++) {
-    fill(255);
-    let agent = new Agent(points[i].x, points[i].y);
-    agents.push(agent);
-  }
+  init();
 }
 
 function draw() {
-  background(0);
+  background(255);
   agents.forEach(spawn);
 }
 
@@ -28,5 +27,52 @@ function spawn(item, index, arr){
   agents[index].behave();
   agents[index].update();
   agents[index].show();
+}
+
+function init(){
+  textSize(256);
+  textLoc = textWidth(txt)/2;
+  points = font.textToPoints(txt, width/2 - textLoc, height/2, 256);
+  for (let i = 0; i < points.length; i++) {
+    fill(255);
+    let agent = new Agent(points[i].x, points[i].y);
+    agents.push(agent);
+  }
+}
+
+function clearArray(){
+  for (let j = 0; agents.length != 0; j++) {
+    agents.pop();
+  }
+}
+
+function keyPressed(){
+  switch(keyCode){
+    case RETURN:
+      noLoop();
+      clearArray();
+      txt = textInput.value();
+      init();
+      loop();
+      break;
+  }
+}
+
+function mousePressed(){
+  dropBomb();
+}
+
+function dropBomb(){
+  push();
+  translate();
+  let bomb = createVector(mouseX, mouseY);
+  for (let k = 0; k < agents.length; k++) {
+    let distance = p5.Vector.sub(bomb, agents[k].pos);
+    let effect = map(distance.mag(), 0, 300, 50, 0, true);
+    console.log(effect);
+    let scared = p5.Vector.sub(agents[k].pos, bomb);
+    scared.setMag(effect);
+    agents[k].force(scared);
+  }
 
 }
