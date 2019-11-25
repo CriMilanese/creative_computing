52e9c44ffef4499073498eb45476d460496a1bc2
CriMilanese <cmilanese@pop-os.localdomain>
2019-11-25 20:52:08 +0900

    fixed text and word position

--- typo_graph/script.js
+++ typo_graph/script.js
@@ -1,9 +1,7 @@
 var font;
-var txt = "Cris";
+var txt;
 let agents = [];
-let textInput;
-let textLoc;
-let points;
+let points = [];
 
 function preload(){
   font = loadFont('Arvo-Bold.ttf');
@@ -12,14 +10,14 @@ function preload(){
 function setup() {
   // use full screen size
   cnv = createCanvas(windowWidth, windowHeight);
-  textInput = createInput('insert here..');
-  textInput.position(50, 50);
+  cnv.mousePressed(dropBomb);
   textFont(font);
+  txt = ['Freedom', 'is not', 'Free'];
   init();
 }
 
 function draw() {
-  background(255);
+  background(0);
   agents.forEach(spawn);
 }
 
@@ -30,9 +28,14 @@ function spawn(item, index, arr){
 }
 
 function init(){
-  textSize(256);
-  textLoc = textWidth(txt)/2;
-  points = font.textToPoints(txt, width/2 - textLoc, height/2, 256);
+  for (var i = 0; i < txt.length; i++) {
+    textSize(256);
+    let textLocX = (width/2) - (textWidth(txt[i])/2);
+    let textLocY = (i+1)*height/3 - 50;
+    console.log(i+' LocX'+': '+textLocX+' '+' LocY'+': '+textLocY);
+    points = points.concat(font.textToPoints(txt[i], textLocX, textLocY, 256));
+  }
+
   for (let i = 0; i < points.length; i++) {
     fill(255);
     let agent = new Agent(points[i].x, points[i].y);
@@ -46,33 +49,13 @@ function clearArray(){
   }
 }
 
-function keyPressed(){
-  switch(keyCode){
-    case RETURN:
-      noLoop();
-      clearArray();
-      txt = textInput.value();
-      init();
-      loop();
-      break;
-  }
-}
-
-function mousePressed(){
-  dropBomb();
-}
-
 function dropBomb(){
-  push();
-  translate();
   let bomb = createVector(mouseX, mouseY);
   for (let k = 0; k < agents.length; k++) {
     let distance = p5.Vector.sub(bomb, agents[k].pos);
     let effect = map(distance.mag(), 0, 300, 50, 0, true);
-    console.log(effect);
     let scared = p5.Vector.sub(agents[k].pos, bomb);
     scared.setMag(effect);
     agents[k].force(scared);
   }
-
 }
