a86478e122c131750521193bddcc3084d70ffa97
CriMilanese <cmilanese@pop-os.localdomain>
2020-10-20 18:53:27 +0200

    retargeting

--- Agent.js
+++ Agent.js
@@ -1,8 +1,8 @@
 /*
-* agents are independent elements on the canvas that have a target
-* at position tx, ty and move towards it
-*/
-function Agent(tx, ty, radius){
+ * agents are independent elements on the canvas that have a target
+ * at position tx, ty and move towards it
+ */
+function Agent(tx, ty, radius) {
   this.pos = createVector(random(gridWidth), random(gridHeight));
   this.target = createVector(tx, ty);
   this.acc = createVector();
@@ -10,38 +10,42 @@ function Agent(tx, ty, radius){
   this.done = false;
   this.r = radius;
 
-  this.updateBehavior = function(mode, speed){
-    switch(mode){
-      case 'wonder':
-        this.vel = p5.Vector.sub(this.target, this.pos);
-        this.vel.normalize().mult(speed).rotate(3);
-        break;
+  this.updateTarget = function(tx, ty) {
+    this.target = createVector(tx, ty);
+  }
+
+  this.updateBehavior = function(mode, speed) {
+    switch (mode) {
       case 'arrive':
         this.vel = p5.Vector.sub(this.target, this.pos);
         this.acc.mult(0.8);
-        if(this.vel.mag() > speed/2){
+        if (this.vel.mag() > speed / 2) {
           this.vel.normalize().mult(speed).add(this.acc);
-        // } else {
-          // this.done = true;
+        } else {
+          this.done = true;
         }
+        break;
+      case 'wonder':
+        this.vel = p5.Vector.sub(this.target, this.pos);
+        this.vel.normalize().mult(speed).rotate(3);
         break;
       default:
         break;
     }
   }
 
-  this.move = function(mode, speed){
+  this.move = function(mode, speed) {
     this.updateBehavior(mode, speed);
     this.pos.add(this.vel);
   }
 
-  this.show = function(){
+  this.show = function() {
     fill(102, 34, 27);
     noStroke();
     ellipse(this.pos.x, this.pos.y, this.r);
   }
 
-  this.applyForce = function(factor){
+  this.applyForce = function(factor) {
     this.acc.add(factor)
   }
 }
